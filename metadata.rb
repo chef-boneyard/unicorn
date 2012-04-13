08084e20c7be8ce6d5528cc1c99ddb6d9a847320
Andrew Crump <andrew@kotirisoftware.com>
2012-04-13 21:42:46 +0100

    Increment version for development.

--- metadata.rb
+++ metadata.rb
@@ -3,5 +3,5 @@ maintainer_email "cookbooks@opscode.com"
 license          "Apache 2.0"
 description      "Installs/Configures unicorn"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.0.0"
+version          "1.0.1"
 recipe "unicorn", "Installs unicorn rubygem"
