7dc049ed76e732136f3f7cc948dc43a11f6dc175
jtimberman <joshua@opscode.com>
2013-02-14 16:48:31 -0700

    release v1.2.0

--- metadata.rb
+++ metadata.rb
@@ -4,5 +4,5 @@ maintainer_email "cookbooks@opscode.com"
 license          "Apache 2.0"
 description      "Installs/Configures unicorn"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.1.0"
+version          "1.2.0"
 recipe "unicorn", "Installs unicorn rubygem"
