e204c806d55131fccff71c1ba57c296b737272e6
Tim Smith <tsmith84@gmail.com>
2015-09-02 09:52:57 -0700

    Update copyrights and remove extra spaces

--- definitions/unicorn_config.rb
+++ definitions/unicorn_config.rb
@@ -3,7 +3,7 @@
 # Cookbook Name:: unicorn
 # Definition:: unicorn_config
 #
-# Copyright 2009, Chef Software, Inc.
+# Copyright 2009-2015, Chef Software, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -18,26 +18,26 @@
 # limitations under the License.
 #
 
-define :unicorn_config, 
-    :listen               => nil, 
+define :unicorn_config,
+    :listen               => nil,
     :working_directory    => nil,
-    :worker_timeout       => 60, 
-    :preload_app          => false, 
+    :worker_timeout       => 60,
+    :preload_app          => false,
     :worker_processes     => 4,
-    :unicorn_command_line => nil, 
-    :forked_user          => nil, 
-    :forked_group         => nil, 
+    :unicorn_command_line => nil,
+    :forked_user          => nil,
+    :forked_group         => nil,
     :pid                  => nil,
     :before_exec          => nil,
-    :before_fork          => nil, 
-    :after_fork           => nil, 
+    :before_fork          => nil,
+    :after_fork           => nil,
     :stderr_path          => nil,
-    :stdout_path          => nil, 
-    :notifies             => nil, 
-    :owner                => nil, 
+    :stdout_path          => nil,
+    :notifies             => nil,
+    :owner                => nil,
     :group                => nil,
-    :mode                 => nil, 
-    :copy_on_write        => false, 
+    :mode                 => nil,
+    :copy_on_write        => false,
     :enable_stats         => false do
 
   config_dir = File.dirname(params[:name])
@@ -46,7 +46,7 @@ define :unicorn_config,
     recursive true
     action :create
   end
-  
+
   template params[:name] do
     source "unicorn.rb.erb"
     cookbook "unicorn"
@@ -57,7 +57,7 @@ define :unicorn_config,
     variables params
     notifies *params[:notifies] if params[:notifies]
   end
-  
+
   # If the user set a group for forked processes but not a user, warn them that
   # we did not set the group. Unicorn does not allow you to drop privileges at
   # the group level only.
