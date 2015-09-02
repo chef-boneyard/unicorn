278944e8ac575c4ab5123812c51e174ed3740c74
Tim Smith <tsmith84@gmail.com>
2015-09-02 09:58:16 -0700

    Add Chef 11 compat and cleanup wording

--- metadata.rb
+++ metadata.rb
@@ -2,10 +2,11 @@ name 'unicorn'
 maintainer 'Chef Software, Inc'
 maintainer_email 'cookbooks@chef.io'
 license 'Apache 2.0'
-description 'Installs/Configures unicorn'
+description 'Installs and configures unicorn'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '2.0.0'
-recipe 'unicorn', 'Installs unicorn rubygem'
+supports 'ubuntu'
+recipe 'unicorn::default', 'Installs unicorn rubygem'
 
-source_url 'https://github.com/opscode-cookbooks/unicorn'
-issues_url 'https://github.com/opscode-cookbooks/unicorn/issues'
+source_url 'https://github.com/opscode-cookbooks/unicorn' if respond_to?(:source_url)
+issues_url 'https://github.com/opscode-cookbooks/unicorn/issues' if respond_to?(:source_url)
